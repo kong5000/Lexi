4a7f72c9c78431b6bec8ab32326b86d9b9d507df
k <keith.ong5000@gmail.com>
2024-02-01 13:18:30 -0300

    added hint timer and hint logic

--- WordGame/ContentView.swift
+++ WordGame/ContentView.swift
@@ -16,156 +16,84 @@ struct ContentView: View {
     @State private var wheelLetters = [[String](),[String](),[String](),[String]()]
     
     
-    @State private var letters1 = [String]()
     @State private var letter1: String = ""
+    @State private var letter2: String = ""
+    @State private var letter3: String = ""
+    @State private var letter4: String = ""
     
-    @State private var letters2 = ["F", "E","H", "I", "D"]
-    @State private var letter2: String = "E"
     
-    @State private var letters3 = ["I", "P","X", "Z", "E"]
-    @State private var letter3: String = "X"
+    @State private var hint1: String? = nil
+    @State private var hint2: String? = nil
+    @State private var hint3: String? = nil
+    @State private var hint4: String? = nil
     
-    @State private var letters4 = ["K", "B","C", "I", "E"]
-    @State private var letter4: String = "I"
+    @State private var hintState = 0
     
     @State private var state: Int = 0
     
     @State private var viewModel = LocalDictionary()
     
+    @State private var countdown = 10
+    @State private var isButtonActive = false
+    private let initialCountdown = 10
+    @State private var countdownTimer: Timer?
+
+    private func startCountdown() {
+        // Invalidate the previous timer if it exists
+        countdownTimer?.invalidate()
+
+        isButtonActive = false
+        countdown = initialCountdown
+
+        countdownTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            if countdown > 0 {
+                countdown -= 1
+            } else {
+                isButtonActive = true
+                timer.invalidate()
+            }
+        }
+    }
+    
+    
     
     var body: some View {
         VStack {
-            Text(viewModel.gameWords[state].hint)
-                .font(.system(size: 20)) // You can adjust the size (e.g., 20) as per your requirement
-            //            Text(viewModel.gameWords[state].word)
+            Text(hint1 ?? "NONE")
+            Text("Score \(solvedWords.count)")
+                .font(.system(size: 30)) // You can adjust the size (e.g., 20) as per your requirement
+                .padding()
             
+        
+            Text(viewModel.gameWords[state].hint)
+                .font(.system(size: 30))
+                .padding(.bottom, 80)
+                .padding(.top, 20)
+            
+                        
             HStack(alignment: .top){
                 VStack{
-//                    Button(action: {
-//                        if(!letters1.contains(freeLetter)){
-//                            letters1.insert(freeLetter, at:0)
-//                        }
-//                        letters2.removeAll { $0 == freeLetter }
-//                        letters3.removeAll { $0 == freeLetter }
-//                        letters4.removeAll { $0 == freeLetter }
-//                    }) {
-//                        if(letters1.contains(freeLetter)){
-//                            Text(freeLetter)
-//                                .font(.system(size: 36))
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }else{
-//                            Color.white
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }
-//                    }.padding(.bottom, 80)
-                    Wheel(selectedLetter: $letter1, freeLetter: freeLetter, letters: wheelLetters[0])
+                    Wheel(selectedLetter: $letter1, freeLetter: freeLetter, letters: wheelLetters[0], hint: $hint1)
                         .padding()
                     Spacer()
                 }
                 VStack{
-//                    Button(action: {
-//                        if(!letters2.contains(freeLetter)){
-//                            letters2.insert(freeLetter, at:0)
-//                        }
-//                        letters1.removeAll { $0 == freeLetter }
-//                        letters3.removeAll { $0 == freeLetter }
-//                        letters4.removeAll { $0 == freeLetter }
-//                    }) {
-//                        if(letters2.contains(freeLetter)){
-//                            Text(freeLetter)
-//                                .font(.system(size: 36))
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }else{
-//                            Color.white
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }
-//                    }.padding(.bottom, 80)
-                    Wheel(selectedLetter: $letter2, freeLetter: freeLetter, letters: wheelLetters[1])
+                    Wheel(selectedLetter: $letter2, freeLetter: freeLetter, letters: wheelLetters[1], hint: $hint2)
                         .padding()
                     Spacer()
                 }
                 VStack{
-//                    Button(action: {
-//                        if(!letters3.contains(freeLetter)){
-//                            letters3.insert(freeLetter, at:0)
-//                        }
-//                        letters2.removeAll { $0 == freeLetter }
-//                        letters1.removeAll { $0 == freeLetter }
-//                        letters4.removeAll { $0 == freeLetter }
-//                    }) {
-//                        if(letters3.contains(freeLetter)){
-//                            Text(freeLetter)
-//                                .font(.system(size: 36))
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }else{
-//                            Color.white
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }
-//                    }.padding(.bottom, 80)
-                    Wheel(selectedLetter: $letter3, freeLetter: freeLetter, letters: wheelLetters[2])
+                    Wheel(selectedLetter: $letter3, freeLetter: freeLetter, letters: wheelLetters[2], hint: $hint3)
                         .padding()
                     Spacer()
                 }
                 VStack{
-//                    Button(action: {
-//                        if(!letters4.contains(freeLetter)){
-//                            letters4.insert(freeLetter, at:0)
-//                        }
-//                        letters2.removeAll { $0 == freeLetter }
-//                        letters3.removeAll { $0 == freeLetter }
-//                        letters1.removeAll { $0 == freeLetter }
-//                    }) {
-//                        if(letters4.contains(freeLetter)){
-//                            Text(freeLetter)
-//                                .font(.system(size: 36))
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }else{
-//                            Color.white
-//                                .frame(width: 70, height: 70)
-//                                .overlay{
-//                                    Rectangle()
-//                                        .stroke(Color.black, lineWidth:3)
-//                                }
-//                        }
-//                    }.padding(.bottom, 80)
-                    Wheel(selectedLetter: $letter4, freeLetter: freeLetter, letters: wheelLetters[3])
+                    Wheel(selectedLetter: $letter4, freeLetter: freeLetter, letters: wheelLetters[3], hint: $hint4)
                         .padding()
                     Spacer()
                 }
                 
             }.frame(maxHeight: .infinity)
-            Text("Score \(solvedWords.count)")
-                .font(.system(size: 30)) // You can adjust the size (e.g., 20) as per your requirement
-                .padding()
-            
 
             HStack{
                 Text(letter1)
@@ -178,6 +106,37 @@ struct ContentView: View {
                     .font(.title)
                 
             }.padding()
+            Button(action: {
+                 if isButtonActive {
+                     startCountdown()
+                     switch hintState {
+                     case 0:
+                         hint1 = ("\(viewModel.gameWords[state].word[0])".capitalized)
+                     case 1:
+                         hint2 = ("\(viewModel.gameWords[state].word[1])".capitalized)
+                     case 2:
+                         hint3 = ("\(viewModel.gameWords[state].word[2])".capitalized)
+                     case 3:
+                         hint4 = ("\(viewModel.gameWords[state].word[3])".capitalized)
+                     default:
+                         print("")
+                     }
+                     hintState += 1
+
+                 }
+             }) {
+                 HStack{
+                     Text("\(countdown)")
+                         .font(.system(size: 20))
+                     Text("Hint")
+                         .padding()
+                         .foregroundColor(.white)
+                         .background(isButtonActive ? Color.blue : Color.gray)
+                         .cornerRadius(10)
+                 }.frame(width: 200)
+      
+             }
+             .disabled(!isButtonActive)
             Button{
                 let combined = letter1 + letter2 + letter3 + letter4
                 if LocalDictionary.shared.isWordInEnglishDictionary(word: combined) {
@@ -187,6 +146,13 @@ struct ContentView: View {
 
                         if(state < viewModel.gameWords.count - 1){
                             state += 1
+                            hintState = 0
+                            hint1 = nil
+                            hint2 = nil
+                            hint3 = nil
+                            hint4 = nil
+                            
+                            startCountdown()
                         }else{
                             showingAlert = true
                         }
@@ -203,11 +169,15 @@ struct ContentView: View {
         }.onAppear(){
             wheelLetters = viewModel.generateLetters()
             letter1 = wheelLetters[0][0]
+
             letter2 = wheelLetters[1][0]
+  
             letter3 = wheelLetters[2][0]
+  
+
             letter4 = wheelLetters[3][0]
+            startCountdown()
             
-            letters1.insert(freeLetter, at:0)
         }
         .alert("WIN", isPresented: $showingAlert) {
             Button("OK", role: .cancel) {
--- WordGame/Wheel.swift
+++ WordGame/Wheel.swift
@@ -12,21 +12,30 @@ struct Wheel: View {
     @State private var onChangedExecuted = false
     var freeLetter: String
     var letters: [String]
+    @Binding var hint: String?
     
     var body: some View {
         VStack{
             Picker("Steps", selection: $selectedLetter) {
-                    ForEach(letters, id: \.self) { letter in
-                        Text("\(letter)")
-                    }
-                }  .scaleEffect(2)
+                ForEach(letters, id: \.self) { letter in
+                    Text("\(letter)")
+                        .foregroundColor(letter == hint ? .blue : .black)
+                        .tag(letter)
+                    
+                }
+            }  .scaleEffect(2)
                 .pickerStyle(.wheel)
-
+        }.onChange(of: hint) {
+            if let newHint = hint {
+                // Programmatically set the selectedLetter to the newHint
+                withAnimation{
+                    selectedLetter = newHint
+                }
+            }
         }
-   
     }
 }
 
 #Preview {
-    Wheel(selectedLetter: .constant("A"), freeLetter: "C", letters: ["A","B","C"])
+    Wheel(selectedLetter: .constant("A"), freeLetter: "C", letters: ["A","B","C"], hint:.constant("A"))
 }
